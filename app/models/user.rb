59f679373d14a322e7d0e55bbcbf7afce517e746
barbara fux <barbara.fux@gmx.at>
2015-11-23 22:07:32 +0100

    changed model so email is valid and unique)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
 	validates_presence_of :email, :password
 
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates_format_of :email, length: { maximum: 255 },
+                 	    format: { with: VALID_EMAIL_REGEX },
+                 	    uniqueness: { case_sensitive: false }
+
 end
