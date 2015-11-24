e8e892b154ee4c56eefa5e455452c47559036616
barbara fux <barbara.fux@gmx.at>
2015-11-24 22:11:45 +0100

    changed password column name, added route for signup

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
    root 'events#index'
    resources :events
    resources :users
+
+   get '/signup' => 'users#new'
    
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151123205145) do
+ActiveRecord::Schema.define(version: 20151124210149) do
 
   create_table "cities", force: :cascade do |t|
     t.string "name",      limit: 255
@@ -32,10 +32,10 @@ ActiveRecord::Schema.define(version: 20151123205145) do
   add_index "events", ["venue_id"], name: "index_events_on_venue_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",      limit: 255
-    t.string   "password",   limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "email",           limit: 255
+    t.string   "password_digest", limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
   create_table "venues", force: :cascade do |t|
