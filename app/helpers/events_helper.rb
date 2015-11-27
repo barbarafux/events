76124a626b1a1562157614beb96d4c94e4f0bb39
barbara fux <barbara.fux@gmx.at>
2015-11-27 16:51:20 +0100

    only layout

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -16,7 +16,6 @@ module EventsHelper
 	end
 
 	def add_to_g_cal(event)
-
 	    #"https://www.google.com/calendar/render?action=TEMPLATE&text=Your+Event+Name&dates=20140127T224000Z/20140320T221500Z&details=For+details,+link+here:+http://www.example.com&location=Waldorf+Astoria,+301+Park+Ave+,+New+York,+NY+10022&sf=true&output=xml"
 	    "http://www.google.com/calendar/render?action=TEMPLATE" +
 	    "&trp=false" +
