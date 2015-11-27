b8c53ee80bf8911fdd32db6f1efb5a61008a8f34
barbara fux <barbara.fux@gmx.at>
2015-11-27 10:40:00 +0100

    fixed uniqueness of email and deleted obsolete code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,12 @@
 class User < ActiveRecord::Base
-	# encoding: utf-8
-	before_save { self.email = email.downcase }
 
-	## VALIDATIONS
-	validates_presence_of :email, :password
+	# VALIDATIONS
 
 	##  EMAIL
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates_format_of :email, 
-                 	    :with => VALID_EMAIL_REGEX,
-                 	    length: { maximum: 255 },
-                 	    uniqueness: { case_sensitive: false }
+	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, uniqueness: { case_sensitive: false }
 
     ## PASSWORD
     has_secure_password
-    validates :password, length: { minimum: 8 }
+    validates :password, length: { minimum: 4 }
 
 end
