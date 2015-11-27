3db7ab9ce9ecaafc5ea389b9c5df873ab50e5419
barbara fux <barbara.fux@gmx.at>
2015-11-27 11:11:34 +0100

    reworked layout for better overview

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ActiveRecord::Base
+	has_secure_password
 
-	# VALIDATIONS
-
-	##  EMAIL
-	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, uniqueness: { case_sensitive: false }
-
-    ## PASSWORD
-    has_secure_password
-    validates :password, length: { minimum: 4 }
+	validates :email,
+				  presence: true,
+				  uniqueness: {
+				  				case_sensitive: false
+				  			  },
+				  format: {
+				  			with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
+				  		   }
+    
+    validates :password,
+    		  	length: { minimum: 4 }
 
 end
