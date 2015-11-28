dcc807cf1d7c2ef9ca45a5c146ad7896d146ee60
barbara fux <barbara.fux@gmx.at>
2015-11-28 12:16:32 +0100

    added create and destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	user = User.find_by_email(params[:email])
+  	if user && user.authenticate(params[:password])
+  		session[:user_id] = user.id
+  		redirect_to root_url, notice: 'Login was succesful'
+  	else
+  		render :new
+  	end
   end
 
   def destroy
+  	session[:user_id] = nil
+  	redirect_to root_url, notice: 'Logout was succesful'
   end
 end
