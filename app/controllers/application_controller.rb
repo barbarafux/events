ab7036e3bc9fc009b682828f382209c5c9e8d5e4
barbara fux <barbara.fux@gmx.at>
2015-12-01 21:39:10 +0100

    added current user & helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   private
   def current_user
-  	User.where(id: session[:user_id].first)
+  	User.where(id: session[:user_id]).first
   end
   helper_method :current_user
 end
