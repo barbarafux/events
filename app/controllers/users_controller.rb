a5ce8aa5f7c5c8e3ac513a05c983b8308db49ea2
barbara fux <barbara.fux@gmx.at>
2015-12-03 22:38:29 +0100

    added before action to user controller to find user by id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:show, :edit, :update, :destroy]
 
 	def show
-		@user = User.find(params[:id])
 	end
 
+	def edit
+	end
 
 	def new
 		@user = User.new
@@ -13,11 +15,28 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		
 		if @user.save
-			redirect_to '/'
+			session[:user_id] = @user.id
+			redirect_to root_url, notice: 'User created.'
 		else
-			render 'new'
+			render :new
+		end
 	end
-end
+
+	def update
+		respond_to do |format|
+			if @user.update(user_params)
+				format.html { redirect_to @user, notice: 'User was updated.' }
+				format.json { render :show, status: :ok, location: @user}
+			else
+				format.html { render :edit }
+				format.json { render json: @user.errors, status: :unprocessable_entity }
+			end
+		end		
+	end
+
+	def destroy
+	end
+
 
 private
 
@@ -25,6 +44,10 @@ private
 		params.require(:user).permit(:email,
 									 :password,
 									 :password_confirmation)
+	end
+
+	def set_user
+		@user = User.find(params[:id])
 	end
 
 end
