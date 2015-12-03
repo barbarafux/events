c25b083a2d54bb144ab0976ffd6282ee8aa5aa21
barbara fux <barbara.fux@gmx.at>
2015-12-03 22:37:01 +0100

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  
-  resources  :sessions, only: [:new, :create, :destroy]
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -11,9 +10,12 @@ Rails.application.routes.draw do
    resources :users
 
    get 'signup' => 'users#new', :as => 'signup'
-   get 'login' => 'sessions#new', :as => 'login'
-   get 'logout' => 'sessions#destroy', :as => 'logout'
    
+   get 'login' => 'sessions#new', :as => 'login'
+   post 'login' => 'sessions#create'
+   get 'logout' => 'sessions#destroy', :as => 'logout'
+
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
