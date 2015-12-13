d58cce7f7acccf40e54286cbe7458bde90cf9dd7
barbara fux <barbara.fux@gmx.at>
2015-12-13 17:29:51 +0100

    added admin column to user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151201200530) do
+ActiveRecord::Schema.define(version: 20151213162446) do
 
   create_table "cities", force: :cascade do |t|
     t.string "name",      limit: 255
@@ -35,8 +35,9 @@ ActiveRecord::Schema.define(version: 20151201200530) do
   create_table "users", force: :cascade do |t|
     t.string   "email",           limit: 255
     t.string   "password_digest", limit: 255
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.datetime "created_at",                                  null: false
+    t.datetime "updated_at",                                  null: false
+    t.boolean  "admin",                       default: false
   end
 
   create_table "venues", force: :cascade do |t|
